5dd93f99d67a3a7321910f65ed7b7be66513b8fc
JunKyus <34362701+kuk941025@users.noreply.github.com>
2020-05-19 02:58:11 +0900

    Register routing using workbox: precache local, google fonts, image files, api request to firebase functions

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,9 +12,63 @@ workbox.routing.registerRoute(
     }),
 );
 
-workbox.precaching.precacheAndRoute([{"revision":"3ec6e0f19d26ab9f2a224ecb3928d669","url":"icons/icon-128x128.png"},{"revision":"79a782b4adffafbe68589f8297e933dc","url":"icons/icon-16x16.png"},{"revision":"da1e5f909bcc07602b5897d79a468463","url":"icons/icon-24x24.png"},{"revision":"d1694d680a1151525b0f2baca377d4db","url":"icons/icon-256x256.png"},{"revision":"ad6c82c195b3e99ee9932a2f9ddda192","url":"icons/icon-32x32.png"},{"revision":"40f08f1450ce4f7a77224f1763dc54f2","url":"icons/icon-512x512.png"},{"revision":"04530015428ca219fc497a9df9553db6","url":"icons/icon-64x64.png"},{"revision":"a1aa1ea042f6cdfb15e1c48469da62d2","url":"index.html"},{"revision":"7ad466b2b771827df693106475dd23d9","url":"manifest.json"},{"revision":"fa1ded1ed7c11438a9b0385b1e112850","url":"robots.txt"}]);
 
-if (workbox){
+//Add google font to workbox
+workbox.routing.registerRoute(
+    ({ url }) => url.origin === 'https://fonts.googleapis.com',
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'google-fonts-stylesheets',
+    })
+)
+
+workbox.routing.registerRoute(
+    ({ url }) => {
+        return url.origin === 'https://fonts.gstatic.com'
+    },
+    new workbox.strategies.CacheFirst({
+        cacheName: 'google-fonts-webfonts',
+        plugins: [
+            new workbox.cacheableResponse.CacheableResponsePlugin({
+                statuses: [0, 200]
+            }),
+            new workbox.expiration.ExpirationPlugin({
+                maxAgeSeconds: 60 * 60 * 24 * 365,
+                maxEntries: 10
+            })
+        ]
+    })
+);
+
+//Cache shopping image
+workbox.routing.registerRoute(
+    ({url, request}) => {
+        console.log(url);
+        return url.origin === "https://shopping-phinf.pstatic.net"
+    },
+    new workbox.strategies.CacheFirst({
+        cacheName: 'shopping-images',
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 100
+            })
+        ]
+    })
+)
+
+//Cache recommends request
+workbox.routing.registerRoute(
+    ({url, request}) => {
+        console.log(url);
+        return url.origin === "https://us-central1-shopnow-118fe.cloudfunctions.net" && url.pathname === "/getRecommends"
+    },
+    new workbox.strategies.CacheFirst({
+        cacheName: 'recommends-cache'
+    })
+)
+
+workbox.precaching.precacheAndRoute([{ "revision": "3ec6e0f19d26ab9f2a224ecb3928d669", "url": "icons/icon-128x128.png" }, { "revision": "79a782b4adffafbe68589f8297e933dc", "url": "icons/icon-16x16.png" }, { "revision": "da1e5f909bcc07602b5897d79a468463", "url": "icons/icon-24x24.png" }, { "revision": "d1694d680a1151525b0f2baca377d4db", "url": "icons/icon-256x256.png" }, { "revision": "ad6c82c195b3e99ee9932a2f9ddda192", "url": "icons/icon-32x32.png" }, { "revision": "40f08f1450ce4f7a77224f1763dc54f2", "url": "icons/icon-512x512.png" }, { "revision": "04530015428ca219fc497a9df9553db6", "url": "icons/icon-64x64.png" }, { "revision": "a1aa1ea042f6cdfb15e1c48469da62d2", "url": "index.html" }, { "revision": "7ad466b2b771827df693106475dd23d9", "url": "manifest.json" }, { "revision": "fa1ded1ed7c11438a9b0385b1e112850", "url": "robots.txt" }]);
+console.log(workbox)
+if (workbox) {
     console.log('registered');
     console.log(workbox);
 }
--- workbox-config.js
+++ workbox-config.js
@@ -1,10 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 
-workbox.setConfig({ debug: true });
-
-self.skipWaiting();
-
 workbox.routing.registerRoute(
     new RegExp(/\.(png|jpg)/),
     new workbox.strategies.StaleWhileRevalidate({
@@ -12,9 +8,63 @@ workbox.routing.registerRoute(
     }),
 );
 
+
+//Add google font to workbox
+workbox.routing.registerRoute(
+    ({ url }) => url.origin === 'https://fonts.googleapis.com',
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'google-fonts-stylesheets',
+    })
+)
+
+workbox.routing.registerRoute(
+    ({ url }) => {
+        return url.origin === 'https://fonts.gstatic.com'
+    },
+    new workbox.strategies.CacheFirst({
+        cacheName: 'google-fonts-webfonts',
+        plugins: [
+            new workbox.cacheableResponse.CacheableResponsePlugin({
+                statuses: [0, 200]
+            }),
+            new workbox.expiration.ExpirationPlugin({
+                maxAgeSeconds: 60 * 60 * 24 * 365,
+                maxEntries: 10
+            })
+        ]
+    })
+);
+
+//Cache shopping image
+workbox.routing.registerRoute(
+    ({ url, request }) => {
+        console.log(url);
+        return url.origin === "https://shopping-phinf.pstatic.net"
+    },
+    new workbox.strategies.CacheFirst({
+        cacheName: 'shopping-images',
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 100
+            })
+        ]
+    })
+)
+
+//Cache recommends request
+workbox.routing.registerRoute(
+    ({ url, request }) => {
+        console.log(url);
+        return url.origin === "https://us-central1-shopnow-118fe.cloudfunctions.net" && url.pathname === "/getRecommends"
+    },
+    new workbox.strategies.CacheFirst({
+        cacheName: 'recommends-cache'
+    })
+)
+
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
-if (workbox){
+if (workbox) {
     console.log('registered');
     console.log(workbox);
 }
