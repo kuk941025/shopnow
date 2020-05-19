f595e878f2b170bf089e773c51fe8c631f416329
JunKyus <34362701+kuk941025@users.noreply.github.com>
2020-05-19 14:27:16 +0900

    Update workbox configuration: add clientsclaim, cache getCategories API, add expiration plugin for getRecommends API

--- workbox-config.js
+++ workbox-config.js
@@ -1,6 +1,38 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 
+workbox.setConfig({ debug: true });
+
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+
+self.skipWaiting();
+
+
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+
+
+workbox.routing.registerRoute(
+    new RegExp(/\.(png|jpg|svg)/),
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'images',
+    }),
+);
+
+
+workbox.routing.registerRoute(
+    ({ url }) => url.origin === 'https://fonts.googleapis.com',
+    new workbox.strategies.CacheFirst({
+        cacheName: 'google-fonts',
+        plugins: [
+
+        ]
+    })
+);
+
+
 workbox.routing.registerRoute(
     new RegExp(/\.(png|jpg)/),
     new workbox.strategies.StaleWhileRevalidate({
@@ -19,6 +51,7 @@ workbox.routing.registerRoute(
 
 workbox.routing.registerRoute(
     ({ url }) => {
+
         return url.origin === 'https://fonts.gstatic.com'
     },
     new workbox.strategies.CacheFirst({
@@ -37,8 +70,7 @@ workbox.routing.registerRoute(
 
 //Cache shopping image
 workbox.routing.registerRoute(
-    ({ url, request }) => {
-        console.log(url);
+    ({ url }) => {
         return url.origin === "https://shopping-phinf.pstatic.net"
     },
     new workbox.strategies.CacheFirst({
@@ -51,20 +83,31 @@ workbox.routing.registerRoute(
     })
 )
 
-//Cache recommends request
+//Cache recommends request: available for 2 days
 workbox.routing.registerRoute(
-    ({ url, request }) => {
-        console.log(url);
+    ({ url }) => {
         return url.origin === "https://us-central1-shopnow-118fe.cloudfunctions.net" && url.pathname === "/getRecommends"
     },
     new workbox.strategies.CacheFirst({
-        cacheName: 'recommends-cache'
+        cacheName: 'recommends-cache',
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 48
+            })
+        ]
     })
 )
 
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
-if (workbox) {
-    console.log('registered');
-    console.log(workbox);
-}
+workbox.routing.registerRoute(
+    ({ url }) => (
+        url.origin === "https://us-central1-shopnow-118fe.cloudfunctions.net" && url.pathname === "/getCategories"
+    ),
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'categories',
+        plugins: [
+
+        ]
+    })
+)
